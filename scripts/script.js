03d2d78831bb40bed22f12d924d92d7fd41be245
Arkadeep <arkadeepsaha11@gmail.com>
2017-11-04 12:40:28 +0530

    improves timing algorithm

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,83 @@
 var worker;
-var minutes=1;// for x minutes set minutes to x-1
+var sessionID = 0;
+
+const WORK_SESSION = 1;
+const BREAK_SESSION = 2;
+const LONG_BREAK_SESSION = 3;
 
 window.onload = function init(){
     document.getElementById("startTimer").addEventListener("click",startTimer);
     document.getElementById("stopTimer").addEventListener("click",stopTimer);
 }
 
-function startTimer(){
+function startTimer()
+{
     console.log("timer started");
+
+    sessionParams.timerLoops[0] = document.getElementById("workSession").value;
+    sessionParams.timerLoops[1] = document.getElementById("numberOfSession").value-1;
+
+    sessionParams.timerLoops[2] = document.getElementById("breakSession").value;
+    sessionParams.timerLoops[3] = document.getElementById("longBreakSession").value;
+
+    sessionID = WORK_SESSION;
     
-    startWorker();
+    startWorker(sessionParams.timerLoops[0]);
 }
 
 function stopTimer()
 {
     worker.terminate();
+    document.getElementById("watchFace").innerHTML="--:--";
 }
 
-function startWorker()
+function startWorker(time)
 {
+    
     //console.log("inside start worker");
     worker = new Worker('scripts/worker.js');
     
     //console.log("worker started")
 
-    worker.postMessage([60]);
+    worker.postMessage([time*60]);
     
     worker.onmessage = onReplyFromWorker;
     //document.getElementById("minutes").innerHTML=y;
 }
 
-function onReplyFromWorker(params){
-    // console.log("printing from main " + parseInt(ev.data)-60);
-    var seconds= parseInt(params.data);
-    //x=x-60;
-    console.log(seconds);
-    document.getElementById("watchFace").innerHTML=(minutes < 10 ? "0" : "")+minutes+"\t:\t"+(seconds < 10 ? "0" : "") + seconds;
-    if(seconds==0)
+function onReplyFromWorker(params)
+{
+    console.log("from script = "+params.data);
+    document.getElementById("watchFace").innerHTML=params.data;
+    if(params.data==="00"+"\t:\t"+"0-1")
     {
-        if(minutes!=0)
-        {
-            worker.terminate();
-            --minutes;
-            startWorker();
-        }
-        else
-        {
-            stopTimer();
-        }
+        stopTimer();
+        nextSession();
+    }
+}
+
+var sessionParams={
+    timerLoops: [0,0,0]
+};
+
+function nextSession()
+{
+    switch(sessionID)
+    {
+        case WORK_SESSION:
+            if(sessionParams.timerLoops[1] != 0)
+            {
+                sessionParams.timerLoops[1]--;
+                sessionID=BREAK_SESSION;
+                startWorker(sessionParams.timerLoops[2]);
+                return;
+            }
+            sessionID=LONG_BREAK_SESSION;
+            startWorker(sessionParams.timerLoops[3]);
+            break;
+        case BREAK_SESSION:
+        case LONG_BREAK_SESSION:
+            sessionID=WORK_SESSION;
+            startWorker(sessionParams.timerLoops[1]);
     }
 }
--- scripts/worker.js
+++ scripts/worker.js
@@ -1,10 +1,20 @@
+var minutes=0;
+var reply;
+
 this.onmessage = function running(params)
 {
     //console.log(params.data + "printing from worker")
-    tickTock(params.data[0]);  
+    tickTock(parseInt(params.data[0]));  
 }
 
 function tickTock(secondsCountdown)
 {
-    setInterval(function(){postMessage(--secondsCountdown)},1000);
+    console.log(reply);
+    setInterval(function()
+    {
+        secondsCountdown--;
+        minutes = parseInt(secondsCountdown/60);
+        console.log(secondsCountdown+"\t::\t"+minutes)
+        reply = (minutes < 10 ? "0" : "")+minutes+"\t:\t"+(secondsCountdown < 10 ? "0" : "") + (secondsCountdown-(minutes*60));
+        postMessage(reply)},1000);
 }
